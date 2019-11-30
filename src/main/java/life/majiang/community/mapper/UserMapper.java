f1b983d410686ab2d0881e0e3a3bacdee9ee9220
liyongzhi <1374309566@qq.com>
2019-11-30 17:51:44 +0800

    11月30日提交，add cookies

---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author 李勇志
+ * @create 2019-11-30 9:48
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
